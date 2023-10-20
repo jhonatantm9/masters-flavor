5ed21a3b606432a7dd23123b484527645ce76e38
Jhonatan <jhonatan.tamayom@udea.edu.co>
2023-10-19 19:23:51 -0500

    Primer commit

---
+++ android/app/src/main/kotlin/com/example/masters_flavor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.masters_flavor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Master\'s Flavor',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          width: 37,
+          child: SvgPicture.asset(
+            'assets/icons/dots-horizontal-svgrepo-com.svg',
+            height: 28,
+            width: 28,
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            child: SvgPicture.asset(
+              'assets/icons/user-svgrepo-com.svg',
+              height: 25,
+              width: 25,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
